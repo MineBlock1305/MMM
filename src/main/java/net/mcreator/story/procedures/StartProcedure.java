fb50503163f20183f1d6efd1708f1b2f9232f937
sashaD12 <apple man@DESKTOP-CR1VG4I>
2023-12-13 17:39:10 +0200

    5

--- src/main/java/net/mcreator/story/procedures/StartProcedure.java
+++ src/main/java/net/mcreator/story/procedures/StartProcedure.java
@@ -1,8 +1,25 @@
 package net.mcreator.story.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import net.mcreator.story.network.StoryModVariables;
+import net.mcreator.story.StoryMod;
 
 import javax.annotation.Nullable;
+
+import java.util.Iterator;
 
 @Mod.EventBusSubscriber
 public class StartProcedure {
@@ -41,8 +58,6 @@ public class StartProcedure {
 					StoryMod.queueServerWork(20, () -> {
 						if (entity instanceof Player _player && !_player.level.isClientSide())
 							_player.displayClientMessage(Component.literal(("[" + entity.getDisplayName().getString() + "]: \u042F... \u042F \u0436\u0438\u0432?")), false);
-						StoryMod.queueServerWork(48000, () -> {
-						});
 					});
 				});
 			}
--- src/main/java/net/mcreator/story/procedures/TimeProcedure.java
+++ src/main/java/net/mcreator/story/procedures/TimeProcedure.java
@@ -1,6 +1,13 @@
 package net.mcreator.story.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
 
 import javax.annotation.Nullable;
 
@@ -23,6 +30,6 @@ public class TimeProcedure {
 		if (entity instanceof Player _player && !_player.level.isClientSide())
 			_player.displayClientMessage(
 					Component.literal("\u044F \u0438\u0449\u043E \u043D\u0435 \u0432\u043E\u0441\u0442\u043E\u043D\u043E\u0432\u0438\u043B \u0440\u043E\u0441\u0443\u0434\u043E\u043A \u0448\u0442\u043E\u0431\u044B \u0441\u043F\u043E\u0442\u044C"),
-					false);
+					true);
 	}
 }
