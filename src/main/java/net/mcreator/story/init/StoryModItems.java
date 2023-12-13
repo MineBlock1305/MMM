aeb613b4dbfb4341571c9bf05ffe6374419f6df5
sashaD12 <apple man@DESKTOP-CR1VG4I>
2023-12-13 17:11:46 +0200

    3

--- src/main/java/net/mcreator/story/init/StoryModItems.java
+++ src/main/java/net/mcreator/story/init/StoryModItems.java
@@ -13,10 +13,12 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 
 import net.mcreator.story.item.RebirthiconItem;
+import net.mcreator.story.item.FItem;
 import net.mcreator.story.StoryMod;
 
 public class StoryModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, StoryMod.MODID);
 	public static final RegistryObject<Item> REBIRTHICON = REGISTRY.register("rebirthicon", () -> new RebirthiconItem());
 	public static final RegistryObject<Item> TEST_SPAWN_EGG = REGISTRY.register("test_spawn_egg", () -> new ForgeSpawnEggItem(StoryModEntities.TEST, -1, -1, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+	public static final RegistryObject<Item> F = REGISTRY.register("f", () -> new FItem());
 }
