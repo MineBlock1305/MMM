546a346d395965a2b509c4f108f7d0b0742feeea
MineBlock1305 <aleks@ALIENWARE>
2023-12-13 22:12:29 +0700

    2

--- src/main/java/net/mcreator/story/procedures/StartProcedure.java
+++ src/main/java/net/mcreator/story/procedures/StartProcedure.java
@@ -35,30 +35,32 @@ public class StartProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
-		if (StoryModVariables.MapVariables.get(world).globalevents == 0) {
-			StoryModVariables.MapVariables.get(world).globalevents = 1;
-			StoryModVariables.MapVariables.get(world).syncData(world);
-			{
-				Entity _ent = entity;
-				_ent.teleportTo(100, 10, 100);
-				if (_ent instanceof ServerPlayer _serverPlayer)
-					_serverPlayer.connection.teleport(100, 10, 100, _ent.getYRot(), _ent.getXRot());
-			}
-			StoryMod.queueServerWork(20, () -> {
-				if (entity instanceof ServerPlayer _player) {
-					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("story:rebirth"));
-					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
-					if (!_ap.isDone()) {
-						Iterator _iterator = _ap.getRemainingCriteria().iterator();
-						while (_iterator.hasNext())
-							_player.getAdvancements().award(_adv, (String) _iterator.next());
-					}
+		StoryMod.queueServerWork(100, () -> {
+			if (StoryModVariables.MapVariables.get(world).globalevents == 0) {
+				StoryModVariables.MapVariables.get(world).globalevents = 1;
+				StoryModVariables.MapVariables.get(world).syncData(world);
+				{
+					Entity _ent = entity;
+					_ent.teleportTo(100, 10, 100);
+					if (_ent instanceof ServerPlayer _serverPlayer)
+						_serverPlayer.connection.teleport(100, 10, 100, _ent.getYRot(), _ent.getXRot());
 				}
 				StoryMod.queueServerWork(20, () -> {
-					if (entity instanceof Player _player && !_player.level.isClientSide())
-						_player.displayClientMessage(Component.literal(("[" + entity.getDisplayName().getString() + "]: \u042F... \u042F \u0436\u0438\u0432?")), false);
+					if (entity instanceof ServerPlayer _player) {
+						Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("story:rebirth"));
+						AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+						if (!_ap.isDone()) {
+							Iterator _iterator = _ap.getRemainingCriteria().iterator();
+							while (_iterator.hasNext())
+								_player.getAdvancements().award(_adv, (String) _iterator.next());
+						}
+					}
+					StoryMod.queueServerWork(20, () -> {
+						if (entity instanceof Player _player && !_player.level.isClientSide())
+							_player.displayClientMessage(Component.literal(("[" + entity.getDisplayName().getString() + "]: \u042F... \u042F \u0436\u0438\u0432?")), false);
+					});
 				});
-			});
-		}
+			}
+		});
 	}
 }
